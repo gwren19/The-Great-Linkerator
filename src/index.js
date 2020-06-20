adefff199381dab606e6c4b41c487bc6d96416cc
Garrett Wren <gwren19@gmail.com>
2020-06-20 10:13:54 -0700

    pushing up working code

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     return (
-        <div id = "App">
+        <div id='app'>
             <h1>The Great Linkerator</h1>
             <p>lorem ipsum</p>
-        </div>
+        </div>    
     )
 }
 
-// const app = document.querySelector('#app');
-
 ReactDOM.render(
-    <App></App>,
-    document.getElementById('root')//getElementById tell you where to put the h1 element. root is the Div in the body of index.html in the public folder
-
-)
+<App />,
+document.getElementById('app')
+);
