a0d59e07db48e6cbe43e9603ed1b381661c180ab
Chris <chrisfyi@hotmail.com>
2020-08-14 22:20:40 -0700

    Design Changes
    Used Bulma and react-bootstrap to react a new look for the project

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,14 +5,16 @@ import './SearchResults.css';
 const SearchResults = ({ results }) => {
     console.log('result:', results)
     return (
-        <div id='results'>
-            <h3 className="resultsTitle"> Here's what we found ({ results.length } results): </h3>
-                {results.map(result => {
-                    console.log('single result:', result)
-                    return (
-                    <Link key={ result.id } { ...result } />)
-                })}
-        </div>
+        <section class="section">
+            <div class="container">
+                <h3 class="title has-text-centered"> Here's what we found ({ results.length } results): </h3>
+                    {results.map(result => {
+                        console.log('single result:', result)
+                        return (
+                        <Link key={ result.id } { ...result } />)
+                    })}
+            </div>
+        </section>
     );
 }
 
